7c4fa51b96df8d447516df762ca0a1525e905286
Artiom Ciobanu <aciobanumain@gmail.com>
2021-09-05 20:17:27 +0300

    Python lab2 finished

--- Semester 5/Python/Lab2/main.py
+++ Semester 5/Python/Lab2/main.py
@@ -1,14 +1,14 @@
-# string = 'String!'
+#string = 'String!'
 # print(f'Hello! here is a string variable: {string}. The type: {type(string)}. The length: {len(string)}')
-#
+
 # print(f'\nThe previous text in uppercase: {string.upper()}')
-#
+
 # print(f'\nLets take a character: {string[5]}')
-#
+
 # print(f'\nLets take a character from the end: {string[-2]}')
-#
+
 # print(f'\nLets take a substring: {string[4:]}')
-# print(f'\nLets take anoter substring: {string[:2:-2]}')
+#print(f'\nLets take anoter substring: {string[:2:-2]}') #Evetything before
 #
 # number = 5
 # print(f'\nIt is a number: {number}. Its type is {type(number)}')
@@ -23,16 +23,29 @@
 # print(f'\nHere is the \'a\' character: {character}')
 
 
-def print_hi(name):
+# def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 txt = "More results from text..."
-substr = txt[4:12]
+substr = txt[4:6] #Substring made of characters from 4th to 6th
 print(substr)
 print(substr.strip())
+
+print("    asdiuahsd asdasd   ".strip()) #Truncates the string
+
+txt = "More results from text..."
+print(txt.split())
+
+
+# stri = "abcdefgh12345678"
+# print(stri[5:]) #From 5 character till the end
+# print(stri[5:-2]) #From 5 to 2 from the end
+# print(stri[-5:])#From 5 from the end to the end
+# print(stri[:10:3])#Skips 3 in substring from begin to 10 character
+# print(stri[:10])#First 10 characters
