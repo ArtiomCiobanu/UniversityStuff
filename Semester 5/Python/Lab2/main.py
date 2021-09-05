3bd446171f573bdac0c41fc7348f51e1c64d7f41
Artiom Ciobanu <aciobanumain@gmail.com>
2021-09-05 18:23:57 +0300

    .

--- Semester 5/Python/Lab2/main.py
+++ Semester 5/Python/Lab2/main.py
@@ -1,26 +1,26 @@
-string = 'String!'
-print(f'Hello! here is a string variable: {string}. The type: {type(string)}. The length: {len(string)}')
-
-print(f'\nThe previous text in uppercase: {string.upper()}')
-
-print(f'\nLets take a character: {string[5]}')
-
-print(f'\nLets take a character from the end: {string[-2]}')
-
-print(f'\nLets take a substring: {string[4:]}')
-print(f'\nLets take anoter substring: {string[:2:-2]}')
-
-number = 5
-print(f'\nIt is a number: {number}. Its type is {type(number)}')
-
-number = string
-print(f'\nLet us change the type of the number variable: {number}')
-
-floatNumber = 5.5
-print(f'\nFloating-point number: {floatNumber}')
-
-character = 'a'
-print(f'\nHere is the \'a\' character: {character}')
+# string = 'String!'
+# print(f'Hello! here is a string variable: {string}. The type: {type(string)}. The length: {len(string)}')
+#
+# print(f'\nThe previous text in uppercase: {string.upper()}')
+#
+# print(f'\nLets take a character: {string[5]}')
+#
+# print(f'\nLets take a character from the end: {string[-2]}')
+#
+# print(f'\nLets take a substring: {string[4:]}')
+# print(f'\nLets take anoter substring: {string[:2:-2]}')
+#
+# number = 5
+# print(f'\nIt is a number: {number}. Its type is {type(number)}')
+#
+# number = string
+# print(f'\nLet us change the type of the number variable: {number}')
+#
+# floatNumber = 5.5
+# print(f'\nFloating-point number: {floatNumber}')
+#
+# character = 'a'
+# print(f'\nHere is the \'a\' character: {character}')
 
 
 def print_hi(name):
@@ -31,3 +31,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+txt = "More results from text..."
+substr = txt[4:12]
+print(substr)
+print(substr.strip())
