d5a60460b49ce3de9951c1bb33ad2b4a8da20c63
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-10-03 19:49:45 +0300

    Lab5 python n1

--- Semester 5/Python/Lab5/main.py
+++ Semester 5/Python/Lab5/main.py
@@ -1,12 +1,48 @@
 itemList = []
 
-def Add(item):
-    pass
+def add(item):
+    if item not in itemList:
+        itemList.append(item)
 
-def Remove(item):
-    pass
 
-def PrintItems():
-    pass
+def remove(itemToAdd):
+    for item in itemList:
+        if item == itemToAdd:
+            itemList.remove(item)
+            break
 
+
+def print_items():
+    for item in itemList:
+        print(item)
+
+
+print("Select one of the following options: ")
+print("0 - exit")
+print("1 - add an item")
+print("2 - remove an item")
+print("3 - print the list")
+
+actionNumber = -1
+while actionNumber != 0:
+    print("\nenter an option:")
+    actionNumber = int(input())
+    if actionNumber > 3 or actionNumber < 0:
+        print("Please select a correct option.")
+    else:
+        print(f"The selected option is: {actionNumber}")
+
+    if actionNumber == 0:
+        print("Exiting...")
+        break
+    if actionNumber == 1:
+        print("Enter an item to add to the list: ")
+        item = input()
+        add(item)
+    elif actionNumber == 2:
+        print("Enter an item to remove from the list: ")
+        item = input()
+        remove(item)
+    elif actionNumber == 3:
+        print_items()
 
