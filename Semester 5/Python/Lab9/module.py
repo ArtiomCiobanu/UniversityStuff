cc348f0b9e958a6c22f1aa38b88ef44addc896be
Artiom Ciobanu <aciobanumain@gmail.com>
2021-10-27 21:12:14 +0300

    lab10 python

--- Semester 5/Python/Lab9/module.py
+++
@@ -1,2 +0,0 @@
-def print_name():
-    print(__name__)
--- Semester 5/Python/Lab9/script1.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from time import sleep
-
-print("Sleeping 2 seconds")
-sleep(2)
-
-print(f"Current date: {datetime.today()}")
-
