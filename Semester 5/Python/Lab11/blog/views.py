3f210dd795b47f426fbeb4559b4a43179446d12c
Artiom Ciobanu <aciobanumain@gmail.com>
2021-11-10 20:39:52 +0200

    Added venv

--- Semester 5/Python/Lab11/blog/views.py
+++ Semester 5/Python/Lab11/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, publish__year=year, publish__month=month, publish__day=day)
+
+    return render(request, 'blog/post/detail.html', {'post': post})
