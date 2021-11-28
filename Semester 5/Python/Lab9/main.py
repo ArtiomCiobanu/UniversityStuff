83faf178b5461506ce3f060796f39599b876ce40
Artiom Ciobanu <aciobanumain@gmail.com>
2021-11-28 15:42:07 +0200

    Added lab9 python

--- Semester 5/Python/Lab9/main.py
+++ Semester 5/Python/Lab9/main.py
@@ -1,6 +1,38 @@
-from module import print_name
+import calendar
+import time
+from datetime import date, datetime
 
-print(__name__)
+now = datetime.now()
+print(f"Here's the current date and time: {now}\n")
 
-print_name()
+year = now.year
+month = now.month
+day = now.day
+print(f"Current year: {year}")
+print(f"Current month: {month}")
+print(f"Current day: {day}")
 
+someDate = date(year, 12, 31)
+print(f"\nNew year's date: {someDate}")
+
+if now == someDate:
+    print("Happy new Year!")
+else:
+    print("Days until new Year:", (someDate - now.date()).days)
+
+print(f"\nHere's the calendar for {year}")
+
+cal = calendar.TextCalendar(calendar.SUNDAY)
+year = cal.formatyear(2021)
+print(year)
+
+print(time.time())
+print(time.strftime("%B %d, %Y"))
+print(time.asctime())
+
+dateOfBirthday = time.strptime(input("Enter your birthday: "), "%d.%m.%Y")
+day = date.today() - date.fromtimestamp(time.mktime(dateOfBirthday))
+age = day / 365.25
+print("Your age:", age.days)
+
+time.sleep(1)
