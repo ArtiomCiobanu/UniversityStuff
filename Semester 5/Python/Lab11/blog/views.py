34143578f8019340d0990a5279ec1f49561482a5
Artiom Ciobanu <aciobanuwork@gmail.com>
2021-11-30 23:46:10 +0200

    .

--- Semester 5/Python/Lab11/blog/views.py
+++ Semester 5/Python/Lab11/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
 
 
 def post_list(request):
@@ -17,7 +19,12 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, publish__year=year, publish__month=month, publish__day=day)
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+# def post_detail(request, year, month, day, post):
+#   post = get_object_or_404(Post, slug=post, publish__year=year, publish__month=month, publish__day=day)
+#  return render(request, 'blog/post/detail.html', {'post': post})
