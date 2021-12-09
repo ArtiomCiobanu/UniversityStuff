a3f54896966aab985a4eab79f24e082c163de28e
Artiom Ciobanu <aciobanuwork@gmail.com>
2021-12-09 15:46:27 +0200

    Individual work python

--- Semester 5/Python/Lab11/blog/urls.py
+++ Semester 5/Python/Lab11/blog/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('answer', views.save_answer, name='save_answer'),
+    path('polls', views.PollListView.as_view(), name='poll_list'),
+    path('<slug:poll_slug>', views.poll_detail, name='poll_detail'),
 
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
 
-    path('polls', views.PollListView.as_view(), name='poll_list'),
-    path('<slug:poll_slug>', views.poll_detail, name='poll_detail')
 ]
