2dc61c5604ab3654f63753ed00e8df6f8c5e0f06
Artiom Ciobanu <aciobanumain@gmail.com>
2022-01-17 18:04:20 +0200

    Lab1 android

--- Semester 6/Android/Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Semester 6/Android/Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.lab1
 
+import android.graphics.Color.red
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun SetRedBackground(view: View) {
+        view.rootView.setBackgroundColor(resources.getColor(R.color.red));
+    }
+
+    fun SetYellowBackground(view: View) {
+        view.rootView.setBackgroundColor(resources.getColor(R.color.yellow));
+    }
+
+    fun SetGreenBackground(view: View) {
+        view.rootView.setBackgroundColor(resources.getColor(R.color.green));
+    }
+
 }
