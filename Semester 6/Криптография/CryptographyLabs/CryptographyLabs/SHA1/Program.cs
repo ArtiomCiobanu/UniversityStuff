4ce90e7b367ce30a7509e9255d87a26da4668df1
Artiom Ciobanu <aciobanuwork@gmail.com>
2022-02-10 20:34:44 +0200

    Лекции по крипте

---
+++ Semester 6/Криптография/CryptographyLabs/CryptographyLabs/SHA1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+var input = "aclanguage_input";
+Console.WriteLine($"Input string: {input}");
+
+var inputBytes = Encoding.UTF8.GetBytes(input);
+
+Console.WriteLine("Encrypting:");
+var encrypted = Encrypt(inputBytes);
+
+Console.WriteLine("\nResult:");
+string decrypted_text = Encoding.UTF8.GetString(encrypted);
+Console.WriteLine(decrypted_text);
+
+Console.ReadKey();
+
+byte[] Encrypt(byte[] inputBytes)
+{
+    uint h0 = 0x67452301;
+    uint h1 = 0xEFCDAB89;
+    uint h2 = 0x98BADCFE;
+    uint h3 = 0x10325476;
+    uint h4 = 0xC3D2E1F0;
+
+
+}
+
